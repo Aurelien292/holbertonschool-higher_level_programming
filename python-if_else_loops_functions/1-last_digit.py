c495e0f8df2fbff68720f1b86077b5d511e1bb56
aurelien292 <9960@holbertonstudents.com>
2025-01-15 16:19:02 +0100

    Test

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 import random
-
 number = random.randint(-10000, 10000)
-
-last_digit = str(number)[-1:]
-last_digit = int(last_digit)
-
-if number < 0 and last_digit != 0:
-    last_digit = -abs(last_digit)
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than
-          6 and not 0")
-else:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+last_d = abs(number) % 10
+if number < 0:
+    last_d *= -1
+if last_d > 5:
+    print("Last digit of {:d} is {:d} and is greater\
+ than 5".format(number, last_d))
+elif last_d == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, last_d))
+elif last_d < 6 and last_d != 0:
+    print("Last digit of {:d} is {:d} and is less\
+ than 6 and not 0".format(number, last_d))
--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print(f"{i:02}", end=", ")
-print(f"{99:02}")
+for number in range(0, 100):
+    if number != 99:
+        print(f"{number:02}", end=", ")
+    else:
+        print(f"{number:02}")

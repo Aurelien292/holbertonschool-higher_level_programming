4cb7617d4b1bb38796a36c0254bb676c164d9366
aurelien292 <9960@holbertonstudents.com>
2025-01-22 09:47:50 +0100

    add 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,9 @@
-def delete_at(my_list=[], idx=0):
+def delete_at(my_list=None, idx=0):
+    # Si my_list est None, l'initialiser à une liste vide
+    if my_list is None:
+        my_list = []
     # Vérifier si l'index est valide (positif et dans la plage de la liste)
     if idx >= 0 and idx < len(my_list):
-        del my_list[idx]
+        del my_list[idx]  # Supprimer l'élément à l'index spécifié
+
     return my_list

56adae810b735810782014d74abd5b97a7176374
aurelien292 <9960@holbertonstudents.com>
2025-01-22 10:00:00 +0100

    Test

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def delete_at(my_list=None, idx=0):
-    if my_list is None:
-        my_list = []
-    # Créer une copie de la liste pour ne pas modifier la liste d'origine
-    my_list_copy = my_list[:]
-    # Si l'index est valide, on supprime l'élément
-    if idx >= 0 and idx < len(my_list_copy):
-        del my_list_copy[idx]
-    # Retourner la copie modifiée
-    return my_list_copy
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    del my_list[idx]
+    return my_list

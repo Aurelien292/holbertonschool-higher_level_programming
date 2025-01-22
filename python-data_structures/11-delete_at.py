2eaf2986302ac889040e794e20ed4bf9bd27e6cb
aurelien292 <9960@holbertonstudents.com>
2025-01-22 09:45:48 +0100

    add 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,5 @@
 def delete_at(my_list=[], idx=0):
-	 # Vérifier si l'index est valide (positif et dans la plage de la liste)
+    # Vérifier si l'index est valide (positif et dans la plage de la liste)
     if idx >= 0 and idx < len(my_list):
-        # Supprimer l'élément à l'index spécifié en utilisant la méthode de coupe (slicing)
         del my_list[idx]
     return my_list

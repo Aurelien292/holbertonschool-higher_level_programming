154c11e73f349b5f9037328b98011ff4c14a6417
aurelien292 <9960@holbertonstudents.com>
2025-01-22 15:26:09 +0100

    add exec 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+def roman_to_int(roman_string):
+    if type(roman_string) is not str:
+        return 0
+    roman_values={
+		'I': 1, 
+  		'V': 5, 
+    	'X': 10, 
+     	'L': 50,
+        'C': 100, 
+        'D': 500, 
+        'M': 1000}
+    total = 0
+    lenght = len(roman_string)
+    for i in range(lenght):
+        current_value = roman_values[roman_string[i]]
+        if i + 1 < lenght and roman_values[roman_string[i + 1]] > current_value:
+            total -= current_value
+        else:
+            total += current_value
+    return total

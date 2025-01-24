afa87ae77e1e7b71f604caa9aa2d9c268c950925
aurelien292 <9960@holbertonstudents.com>
2025-01-24 12:40:51 +0100

    advance

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    sommes_des_produits = 0
+    sommes_des_poids = 0
+
+    for valeur, poids in my_list:
+        sommes_des_produits += valeur * poids
+        sommes_des_poids += poids
+
+    if sommes_des_poids == 0:
+        return 0
+    return sommes_des_produits / sommes_des_poids

ffaf489d17e7e854bdb9a9d62f1c38f5cc3a01f8
aurelien292 <9960@holbertonstudents.com>
2025-01-24 12:51:32 +0100

    advance 102

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    while value in a_dictionary.values():
+        for Key, Valeur in a_dictionary.items():
+            if Valeur == value:
+                del a_dictionary[Key]
+                break
+    return a_dictionary

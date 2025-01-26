894fe0023581a9e88149ca2d3b8751b6de597c25
aurelien292 <9960@holbertonstudents.com>
2025-01-26 16:25:55 +0100

    add 4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Ce module a une fonction def print_square.
+Fonction acceptant les int pour controler une taille
+"""
+
+
+def print_square(size):
+
+    """Si l'argument n'est pas un int ,
+    elle lève une exception TypeError ou
+    si l'argument est plus petit que 0 ValueError.
+    Autrement affiche des "#" .
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    print(("#" * size + '\n') * size, end='')

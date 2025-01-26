1d70b431bde17d83eae6119b33f48719d206f56e
aurelien292 <9960@holbertonstudents.com>
2025-01-26 16:40:52 +0100

    test 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""Ce module contient une fonction appelée text_indentation.
+Elle permet de diviser un texte en plusieurs lignes à chaque fois
+qu'un des caractères de ponctuation ., ?, ou : est rencontré.
+Il supprime également les espaces inutiles avant et après chaque ligne. """
+
+
 def text_indentation(text):
+    """La fonction text_indentation prend une chaîne de texte en entrée et
+    sépare cette chaîne en lignes. Elle recherche les caractères de ponctuation
+    ., ? et : pour déterminer où diviser le texte. Si des espaces superflus
+    existent avant ou après ces divisions, ils sont supprimés."""
     if type(text) is not str:
         raise TypeError("text must be a string")
     line = ""

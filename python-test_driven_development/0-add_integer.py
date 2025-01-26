901a8b1ce0319c16a5969a65bcd16e02fe445ead
aurelien292 <9960@holbertonstudents.com>
2025-01-26 15:13:49 +0100

    test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Ce module a une fonction def add_integer """
+"""
+Ce module a une fonction def add_integer.
+Fonction acceptant des int et des float ,retourne la somme
+de a et b une fois convertie en int
+"""
 
 
 def add_integer(a, b=98):
 
-    """Fonction acceptant des int et des float ,retourne la somme
-    de a et b une fois convertie en int """
+    """Si l'un des arguments n'est pas un entier ou un flottant,
+    elle lève une exception TypeError. Les flottants sont convertis en entiers avant 
+    de retourner la somme des deux valeurs. """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:

84d756fffc9f6ed1c3624c9da8f2d84b2d8627f5
aurelien292 <9960@holbertonstudents.com>
2025-01-26 16:09:31 +0100

    test

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,16 @@
 #!usr/bin/python3
+"""
+Ce module a une fonction def say_my_name.
+Fonction acceptant les str pour first name et last name
+si first name ou last name n'est pas un string retourn une erreur
+"""
+
+
 def say_my_name(first_name, last_name=""):
+
+    """Si l'un des arguments n'est pas une chaine de charactere,
+    elle lève une exception TypeError. Autrement affiche
+    first name ou last name ou les deux . """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:

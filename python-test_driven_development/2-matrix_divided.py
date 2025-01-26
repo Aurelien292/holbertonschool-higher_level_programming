e424eab3254f8c5132e99727e451ace959f5551a
aurelien292 <9960@holbertonstudents.com>
2025-01-26 17:27:25 +0100

    test 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,17 +9,20 @@ def matrix_divided(matrix, div):
     """
     # Vérifie si 'matrix' est bien une liste
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists)\
+                        of integers/floats")
 
     # Vérifie si chaque ligne de 'matrix' est une liste
     for row in matrix:
         if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists)\
+                            of integers/floats")
 
         # Vérifie si chaque élément de la ligne est un int ou un float
         for i in row:
             if type(i) is not int and type(i) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists)\
+                                of integers/floats")
 
     # Vérifie que 'div' est un nombre (int ou float)
     if type(div) is not int and type(div) is not float:
@@ -29,5 +32,6 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # Divise chaque élément de la matrice par div et arrondit le résultat à 2 décimales
+    # Divise chaque élément de la matrice par div et arrondit
+    # le résultat à 2 décimales
     return [[round(i / div, 2) for i in row] for row in matrix]

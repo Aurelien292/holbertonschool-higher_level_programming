f0af25a67d42920487fbcd7282f9e4f40ce2679c
aurelien292 <9960@holbertonstudents.com>
2025-01-27 12:49:42 +0100

    Add exe

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Une classe qui définit un carré.
+
+    Attributs:
+        size (int): La taille du côté du carré. Par défaut, elle est égale à 0.
+
+    Méthodes:
+        __init__(size=0): Initialise le carré avec une taille donnée.
+    """
     pass

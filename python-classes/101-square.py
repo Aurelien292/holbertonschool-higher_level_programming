bc9680dff5fe2dd24983a7036064ce0e044de00b
aurelien292 <9960@holbertonstudents.com>
2025-01-30 20:24:24 +0100

    test 101

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -118,7 +118,7 @@ class Square:
     def __str__(self):
         result = []
         if self.__size == 0:
-            return "\n" * self.__position[1]
+            return ""
         for _ in range(self.__position[1]):
             result.append("")
         for _ in range(self.__size):

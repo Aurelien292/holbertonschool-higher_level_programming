63be663d525d0b5660a593f7824503e5c52389e1
aurelien292 <9960@holbertonstudents.com>
2025-01-30 15:30:54 +0100

    add ex

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -141,7 +141,7 @@ class Rectangle:
         """
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join([str(Rectangle.print_symbol) * self.width
+        return "\n".join([str(self.print_symbol) * self.width
                           for _ in range(self.height)])
 
     def __repr__(self):

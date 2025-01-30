da908cfdf461f0579c63c8418248a578f9cba0ed
aurelien292 <9960@holbertonstudents.com>
2025-01-30 12:21:17 +0100

    add ex 0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 class Rectangle:
+    """
+    Représente un rectangle.
+
+    Cette classe permet de créer un rectangle. Actuellement, elle ne contient
+    aucune fonctionnalité, mais elle peut être étendue pour inclure des
+    attributs
+    et des méthodes comme la largeur, la hauteur, l'aire, le périmètre, etc.
+    """
     pass

72889e5d7a75aae45c669c8e4247c5425bc7149a
aurelien292 <9960@holbertonstudents.com>
2025-02-03 17:28:47 +0100

    Add 8 et 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+"""
+Cree une class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
+    """
+    lève une exception pour indiquer
+    que la méthode n'est pas implémentée
+    dans la class de base.
+    """
+
+    def integer_validator(self, name, value):
+        """
+        Valide si la valeur est un entier supérieur à 0.
+        Si la valeur n'est pas un entier, lève une exception TypeError.
+        Si la valeur est inférieure ou égale à 0, lève une exception
+        ValueError.
+
+        Arguments :
+        name -- Le nom de l'attribut (toujours une chaîne de caractères).
+        value -- La valeur à valider (doit être un entier supérieur à 0).
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    Classe Rectangle qui hérite de BaseGeometry.
+
+    Cette classe représente un rectangle et utilise la validation des entiers
+    pour la largeur et la hauteur via la méthode `integer_validator`
+    de la classe de base.
+
+    Attributs :
+    - __width (int): Largeur du rectangle, validée comme étant un entier
+    positif.
+    - __height (int): Hauteur du rectangle, validée comme étant un entier
+    positif.
+
+    Méthodes :
+    - __init__(width, height) : Initialise un rectangle avec une largeur
+    et une hauteur valides.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialise un objet Rectangle avec la largeur et la hauteur valides.
+
+        Cette méthode valide la largeur et la hauteur à l'aide de
+        `integer_validator`
+        avant de les assigner aux attributs privés `__width` et `__height`.
+
+        Arguments :
+        - width (int): La largeur du rectangle.
+        - height (int): La hauteur du rectangle.
+
+        Lève :
+        - TypeError: Si `width` ou `height` ne sont pas des entiers.
+        - ValueError: Si `width` ou `height` sont inférieurs ou égaux à zéro.
+        """
+
+        self.__width = width
+        self.__height = height
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """
+    Calcule l'aire du rectangle.
+
+    Cette méthode utilise la largeur et la hauteur du rectangle pour
+    retourner le résultat du calcul de l'aire : largeur * hauteur.
+
+    Retourne :
+        int: L'aire du rectangle.
+    """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+    Retourne une chaîne représentant le rectangle.
+
+    Cette méthode retourne une chaîne formatée décrivant le rectangle
+    au format suivant : [Rectangle] <largeur>/<hauteur>.
+
+    Retourne :
+        str: Une chaîne représentant le rectangle avec ses dimensions.
+    """
+
+        return f"[Rectangle] {self.__width}/{self.__height}"

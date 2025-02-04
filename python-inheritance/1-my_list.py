9478c75d3ab613b066352cf2c4297a9d832361a2
aurelien292 <9960@holbertonstudents.com>
2025-02-04 12:39:43 +0100

    add 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,26 +3,14 @@
 Affiche la liste triée en ordre croissant.
 """
 
+
 class MyList(list):
     """
-    Classe héritant de list, avec une méthode print_sorted pour afficher la liste triée.
+    Classe héritant de list, avec une méthode print_sorted
+    pour afficher la liste triée.
     """
     def print_sorted(self):
         """
-        Trie la liste par insertion et affiche la liste triée en ordre croissant.
+        Affiche la liste triée en ordre croissant.
         """
-        # On copie la liste pour ne pas modifier l'originale
-        new_list = self[:]
-        
-        # Tri par insertion (algorithme simple)
-        for i in range(1, len(new_list)):
-            key = new_list[i]
-            j = i - 1
-            # Déplacer les éléments plus grands que key d'une position vers la droite
-            while j >= 0 and key < new_list[j]:
-                new_list[j + 1] = new_list[j]
-                j -= 1
-            new_list[j + 1] = key
-        
-        # Affichage de la liste triée
-        print(new_list)
+        print(sorted(self))

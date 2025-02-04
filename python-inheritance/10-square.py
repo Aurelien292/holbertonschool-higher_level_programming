85cc85a0cad3b511c7773395aecd5899b67cd7a0
aurelien292 <9960@holbertonstudents.com>
2025-02-04 11:06:45 +0100

    add 11

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,6 +5,22 @@
 
 
 class Rectangle:
+    
+    def integer_validator(self, name, value):
+        """
+        Valide si la valeur est un entier supérieur à 0.
+        Si la valeur n'est pas un entier, lève une exception TypeError.
+        Si la valeur est inférieure ou égale à 0, lève une exception
+        ValueError.
+
+        Arguments :
+        name -- Le nom de l'attribut (toujours une chaîne de caractères).
+        value -- La valeur à valider (doit être un entier supérieur à 0).
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
     """
     Classe représentant un rectangle.
 
@@ -23,6 +39,8 @@ class Rectangle:
             width (int): La largeur du rectangle.
             height (int): La hauteur du rectangle.
         """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.width = width
         self.height = height
 

5688af6f12ebf30b8e2527a94ded9ddda591fb8a
aurelien292 <9960@holbertonstudents.com>
2025-02-04 16:17:42 +0100

    add 11

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,38 +6,7 @@
 """
 
 
-class Rectangle:
-    """
-    Classe représentant un rectangle.
-
-    Attributs:
-        width (int): La largeur du rectangle.
-        height (int): La hauteur du rectangle.
-
-    Méthodes:
-        area(): Calcule et retourne l'aire du rectangle.
-    """
-    def __init__(self, width, height):
-        """
-        Initialise un rectangle avec une largeur et une hauteur données.
-
-        Paramètres:
-            width (int): La largeur du rectangle.
-            height (int): La hauteur du rectangle.
-        """
-        self.width = width
-        self.height = height
-
-    def area(self):
-        """
-        Calcule et retourne l'aire du rectangle.
-
-        Retourne:
-            int: L'aire du rectangle (width * height).
-        """
-        return self.width * self.height
-
-
+Rectangle = __import__('9-rectangle').Rectangle
 """
     Classe représentant un carré, héritée de Rectangle.
 """
@@ -72,38 +41,3 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
-
-    def area(self):
-        """
-        Calcule et retourne l'aire du carré.
-
-        Retourne:
-            int: L'aire du carré (width * height), ici c'est size * size.
-        """
-        return self.width * self.height
-
-    def integer_validator(self, name, value):
-        """
-        Valide que la valeur est un entier positif.
-
-        Paramètres:
-            name (str): Le nom de la variable (ici, "size").
-            value (int): La valeur à valider.
-
-        Lève:
-            TypeError: Si value n'est pas un entier.
-            ValueError: Si value est inférieur ou égal à 0.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be a positive integer")
-
-    def __str__(self):
-        """
-        Retourne une chaîne de caractères représentant le carré.
-
-        Retourne:
-            str: Une chaîne du type "[Rectangle] size/size".
-        """
-        return f"[Rectangle] {self.width}/{self.height}"

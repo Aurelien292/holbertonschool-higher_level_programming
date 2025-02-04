a6ac08386e4611d7ee79aaa5d2ccf7268d358817
aurelien292 <9960@holbertonstudents.com>
2025-02-04 12:11:14 +0100

    add 10

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,13 +3,14 @@
 Affiche la liste triée en ordre croissant.
 """
 
+
 class MyList(list):
     """
-    Classe héritant de list, avec une méthode print_sorted pour afficher la liste triée.
+    Classe héritant de list, avec une méthode print_sorted
+    pour afficher la liste triée.
     """
     def print_sorted(self):
         """
         Affiche la liste triée en ordre croissant.
         """
-        self.sort()  # Trie la liste en place
-        print(self)
+        print(sorted(self))

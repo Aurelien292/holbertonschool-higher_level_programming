0a595a2d0f8722a292a28381c2b7583c9ba2d351
aurelien292 <9960@holbertonstudents.com>
2025-02-07 12:41:12 +0100

    add 100

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 class MyInt(int):
+    """
+    Classe MyInt qui hérite de la classe int, mais avec les opérateurs `==`
+    et `!=` inversés.
+    """
 
     def __eq__(self, other):
+        """Inverser l'opérateur d'égalité"""
         return not super().__eq__(other)
 
     def __ne__(self, other):
+        """Inverser l'opérateur d'inégalité"""
         return not super().__ne__(other)

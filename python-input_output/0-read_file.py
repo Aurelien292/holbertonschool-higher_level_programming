d24a5c0606e5c5ff752e3cb04d5444229a9d7cbe
aurelien292 <9960@holbertonstudents.com>
2025-02-10 11:33:26 +0100

    Add 0-6

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
+    """
+    Reads the content of a text file (UTF-8 encoded) and prints it
+    to standard output.
+
+    Args:
+        filename (str): The name of the file to read. Defaults to
+        an empty string,
+                         which will not work as expected. Provide
+                         a valid file path.
+
+    Returns:
+        None: This function does not return any value. It only
+        prints the content of
+              the file to the standard output.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist or
+        cannot be found.
+        IOError: If the file cannot be opened or read for any other reason.
+
+    Example:
+        read_file("example.txt")
+    """
     with open(filename, "r", encoding="UTF8") as file:
         print(file.read(), end="")

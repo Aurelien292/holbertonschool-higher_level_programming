bc6f3b424a8e50a1014a4bb8416912286f0f88f7
aurelien292 <9960@holbertonstudents.com>
2025-02-14 11:05:29 +0100

    modif task 12 input

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python3
-"""
-Fonction qui retourne un triangle pascal
-"""
+"""Module containing pascal_triangle func"""
 
 
 def pascal_triangle(n):
-    # Si n <= 0, on retourne une liste vide
+    """Function to return Pascal's triangle"""
+    # Checking n <= 0 case
     if n <= 0:
         return []
-    """
-Retourne une liste vide si ( n )  est plus petit ou égale à 0
-"""
 
-    # Initialisation du triangle avec la première ligne
-    triangle = [[1]]
-
-    # On génère les autres lignes du triangle
+    # Creating the triangle lists
+    final = [[1]]
     for i in range(1, n):
-        # On commence chaque ligne par [1]
-        row = [1]
-        # On calcule les éléments du milieu de la ligne
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        # On termine la ligne par [1]
-        row.append(1)
-        # On ajoute la ligne au triangle
-        triangle.append(row)
-        """
-Retourne list int pour representé un triangle Pascal
-"""
-    return triangle
+        new_list = []
+        for index in range(i + 1):
+            if index != 0 and index != i:
+                new_list.append(final[i - 1][index - 1] + final[i - 1][index])
+            else:
+                new_list.append(1)
+        final.append(new_list)
+    return final

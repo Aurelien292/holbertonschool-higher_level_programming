6d37ecde3fd11778b9cab710da975953a087e38e
aurelien292 <9960@holbertonstudents.com>
2025-02-19 12:22:44 +0100

    test

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,8 +1,12 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import http.server 
 import json
+import socketserver
 
 
-class SimpleAPIHandler(BaseHTTPRequestHandler):
+PORT = 8000
+
+
+class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
@@ -17,9 +21,9 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
                 "city": "New York"
             }
             self.send_response(200)
-            self.headers('Content-type', 'application.json')
+            self.headers('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(data).encode('utf-8'))
+            self.wfile.write(json.dumps(data).encode())
 
         elif self.path == "/status":
             self.send_response(200)
@@ -31,15 +35,9 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(b"404 not found")
+            self.wfile.write(b"Endpoint not found")
 
 
-def run(server_class=HTTPServer, handler_class=SimpleAPIHandler):
-    server_adress = ('', 8000)
-    httpd = server_class(server_adress, handler_class)
-    print(f"Server running on port {8000}...")
+with socketserver.TCPServer(("", PORT), SimpleAPIHandler) as httpd:
+    print(f"Starting server on port {PORT}")
     httpd.serve_forever
-
-
-if __name__ == "__main__":
-    run()

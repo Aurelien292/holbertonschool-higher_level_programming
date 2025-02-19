f49dea99567b8e0ff4cef1eaf9674c4d1271ff6b
aurelien292 <9960@holbertonstudents.com>
2025-02-19 11:00:41 +0100

    add 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.headers('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            self.send_response(200)
+            self.headers('Content-type', 'application.json')
+            self.end_headers()
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"404 not found")
+
+
+def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=8000):
+    server_adress = ('', port)
+    httpd = server_class(server_adress, handler_class)
+    print(f"Server running on port {port}...")
+    httpd.serve_forever
+
+
+if __name__ == "__main__":
+    run()

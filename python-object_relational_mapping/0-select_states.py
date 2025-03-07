0d87a48490999f94a72e0a962b48b06a980ed614
root <root@DESKTOP-BLAO4BF>
2025-03-07 16:50:11 +0100

    add exs

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,8 @@ Script qui répertorie tous les états de la base de données.
 import MySQLdb
 import sys
 
-def connectBDD(user, password, BDD_name):
+
+def connectBDD(user, password, db):
     """
     Établit une connexion à la base de données MySQL.
 
@@ -14,7 +15,8 @@ def connectBDD(user, password, BDD_name):
     mot de passe et nom de la base de données).
 
     Args:
-        user (str): Le nom d'utilisateur pour se connecter à la base de données.
+        user (str): Le nom d'utilisateur pour se connecter à la base
+        de données.
         password (str): Le mot de passe associé à l'utilisateur.
         BDD_name (str): Le nom de la base de données à laquelle se connecter.
 
@@ -25,26 +27,28 @@ def connectBDD(user, password, BDD_name):
         host="localhost",
         user=user,
         password=password,
-        db=BDD_name,
+        db=db,
         port=3306,
         charset="utf8"
     )
     return connect
 
 # Récupère les arguments passés en ligne de commande
-if __name__== "__main__":
+
+
+if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
-    BDD_name = sys.argv[3]
+    db = sys.argv[3]
 
-connect = connectBDD(user, password, BDD_name)
+connect = connectBDD(user, password, db)
 # Crée un curseur pour exécuter la requête SQL
 cursor = connect.cursor()
 cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
 # Récupère et affiche toutes les lignes de la requête
 ligne_requete = cursor.fetchall()
 for ligne in ligne_requete:
-	print(ligne)
+    print(ligne)
 # Ferme le curseur et la connexion à la base de données
 cursor.close()
 connect.close()

739a275c29ac7f4c652db0355f298f738d9c939c
aurelien292 <9960@holbertonstudents.com>
2025-04-22 13:44:52 +0200

    Add Variable Annotation

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+import math
+
+def floor(n: float) -> int:
+	"""
+    Retourne le plancher (la partie entière inférieure) du nombre flottant n.
+    
+    Args:
+        n (float): Le nombre flottant dont on souhaite obtenir le plancher.
+        
+    Returns:
+        int: La partie entière inférieure de n.
+    """
+	return int(math.floor(n))
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+def to_str(n: float) -> str:
+	"""
+    Convertit un nombre flottant en une chaîne de caractères.
+
+    Args:
+        n (float): Le nombre flottant à convertir en chaîne.
+
+    Returns:
+        str: La représentation en chaîne de caractères du nombre flottant.
+    """
+	return str(n)
